de554173c834753e62fb6071ccab4c34b5908003
unknown <yakovlev.drezna.stydy@mail.ru>
2023-02-02 16:27:59 +0300

    whileShort

--- alert.js
+++ alert.js
@@ -22,14 +22,21 @@
 // let myLogin = prompt("");
 // alert(result);
 
-let counter = 0;
-while (counter < 5) {
-  console.log(counter);
-  counter++;
-}
+// let counter = 0;
+// while (counter < 5) {
+//   console.log(counter);
+//   counter++;
+// }
 
-let reCounter = 15;
-while (reCounter != 0) {
-  console.log(`reCounter = ${reCounter}`);
-  reCounter--;
+// let reCounter = 15;
+// while (reCounter != 0) {
+//   console.log(`reCounter = ${reCounter}`);
+//   reCounter--;
+// }
+
+let shortCounter = 5;
+while (shortCounter) {
+  console.log(`short = ${shortCounter}`);
+  shortCounter--;
 }
+while (shortCounter) console.log(shortCounter--);
