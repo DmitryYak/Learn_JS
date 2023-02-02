71ce1b55ac35e5d60807e11081554cd93861ac7f
unknown <yakovlev.drezna.stydy@mail.ru>
2023-02-02 15:59:06 +0300

    add file alert

---
+++ alert.js
@@ -0,0 +1,23 @@
+// alert("Hello world from file!");
+
+// result = prompt(title, "buy");
+
+// let result = prompt("What is your name");
+// alert(result);
+
+// let age = 19;
+// if (age >= 18 && age >= 90) {
+//   console.log("good");
+// }
+
+// age = 12;
+// if (!(age >= 18 || age >= 90)) {
+//   console.log("good");
+// }
+
+// let isBoss = confirm("Ты здесь главный?");
+
+// alert(isBoss);
+
+let myLogin = prompt("");
+alert(result);
